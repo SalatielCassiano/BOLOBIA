21ba2c2142020fe5d951e2bf8abe304c5142eff7
Salatiel Cassiano <134166531+SalatielCassiano@users.noreply.github.com>
2023-10-15 16:03:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function iniciarConversa() {
+    // Substitua o número de telefone por seu número no formato internacional
+    window.location.href = 'https://wa.me/551234567890';
+}
+function iniciarConversa() {
+    // Substitua o número de telefone por seu número no formato internacional
+    window.location.href = 'https://wa.me/551234567890';
+}
